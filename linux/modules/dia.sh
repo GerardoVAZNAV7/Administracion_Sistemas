b4709c86235b1bcc25e3ec9d4e01e8516c746839
GerardoVAZNAV7 <gvazquez020705@gmail.com>
2026-03-12 00:26:11 -0700

    FIX: Script de diagnostico prueba

--- linux/modules/dia.sh
+++ linux/modules/dia.sh
@@ -1,14 +1,21 @@
-# 1. Ver si SELinux esta bloqueando
-sudo ausearch -m avc -ts recent 2>/dev/null | grep vsftpd | tail -20
+# # 1. Ver si SELinux esta bloqueando
+# sudo ausearch -m avc -ts recent 2>/dev/null | grep vsftpd | tail -20
 
-# 2. Ver estado actual de los booleanos FTP
-getsebool -a | grep ftp
+# # 2. Ver estado actual de los booleanos FTP
+# getsebool -a | grep ftp
 
-# 3. Ver contexto SELinux de la carpeta del grupo
-ls -lZd /srv/ftp/groups/reprobados
-ls -lZd /home/t1/reprobados
+# # 3. Ver contexto SELinux de la carpeta del grupo
+# ls -lZd /srv/ftp/groups/reprobados
+# ls -lZd /home/t1/reprobados
 
-# 4. Ver permisos reales y ACLs
-ls -la /home/t1/
-getfacl /srv/ftp/groups/reprobados
-id t1
+# # 4. Ver permisos reales y ACLs
+# ls -la /home/t1/
+# getfacl /srv/ftp/groups/reprobados
+# id t1
+
+sudo setfacl    -m "g:ftp-users:rwx" /srv/ftp/groups/reprobados
+sudo setfacl -d -m "g:ftp-users:rwx" /srv/ftp/groups/reprobados
+sudo setfacl    -m "g:ftp-users:rwx" /srv/ftp/groups/recursadores
+sudo setfacl -d -m "g:ftp-users:rwx" /srv/ftp/groups/recursadores
+sudo setfacl    -m "g:ftp-users:rwx" /srv/ftp/general
+sudo setfacl -d -m "g:ftp-users:rwx" /srv/ftp/general
