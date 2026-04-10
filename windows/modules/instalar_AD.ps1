9ed72366d4065f7499b1de35d32a26dd92afe56e
GerardoVAZNAV7 <gvazquez020705@gmail.com>
2026-04-10 12:35:46 -0700

    FIX: AD Practica 8

--- windows/modules/instalar_AD.ps1
+++ windows/modules/instalar_AD.ps1
@@ -3,4 +3,4 @@ Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
 # 2. Crear el nuevo bosque (Cambia "practica8.local" por el nombre que quieras)
 # NOTA: Esto reiniciará el servidor automáticamente al terminar.
-Install-ADDSForest -DomainName "practica8.local" -SafeModeAdministratorPassword (ConvertTo-SecureString "DRAGONBALLz1234!!" -AsPlainText -Force) -Force
+Install-ADDSForest -DomainName "baja.com" -SafeModeAdministratorPassword (ConvertTo-SecureString "Gerardo1234!!" -AsPlainText -Force) -Force
