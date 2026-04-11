0671364f3e74d65474e4b33dda20654fd465677c
GerardoVAZNAV7 <gvazquez020705@gmail.com>
2026-04-10 18:14:25 -0700

    FIX: AD Practica 8

--- windows/modules/carpetas.ps1
+++ windows/modules/carpetas.ps1
@@ -1,15 +1,7 @@
-# Compartir la carpeta Perfiles en red
-New-SmbShare -Name "Perfiles" `
-    -Path "C:\Perfiles" `
-    -FullAccess "BAJA\Administrators" `
-    -ChangeAccess "BAJA\Domain Users"
-
-    $usuarios = Import-Csv "C:\Users\Administrator\Administracion_Sistemas\windows\modules\usuarios.csv"
-
+$usuarios = Import-Csv "C:\Users\Administrator\Administracion_Sistemas\windows\modules\usuarios.csv"
 foreach ($u in $usuarios) {
-    $nombre = $u.Usuario
-    $depto = $u.Departamento -replace " ", ""
-    
+    $nombre = $u.usuario.Trim()
+    $depto  = $u.departamento.Trim() -replace " ", ""
     Set-ADUser -Identity $nombre `
         -HomeDirectory "\\192.168.10.10\Perfiles\$depto\$nombre" `
         -HomeDrive "H:"
