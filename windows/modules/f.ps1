7b270763929cd0b8356329636f11f95e867096a9
GerardoVAZNAV7 <gvazquez020705@gmail.com>
2026-04-22 02:15:22 -0700

    FIX: Usuario Admin

--- windows/modules/f.ps1
+++ windows/modules/f.ps1
@@ -1,5 +1,8 @@
-# Abrir politica local
+# Crear la carpeta temp
+New-Item -ItemType Directory -Path "C:\temp" -Force
+
+# Exportar politica
 secedit /export /cfg C:\temp\sec.cfg /areas USER_RIGHTS
 
-# Ver quien tiene el derecho de red actualmente
+# Ver el derecho actual
 Get-Content C:\temp\sec.cfg | Select-String "SeNetworkLogonRight"
